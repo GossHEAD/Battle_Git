15c3b5f05f7c384935f24cc5bfcd7f79e94f97f8
GossHEAD <bagdachov@gmail.com>
2021-10-20 22:41:05 +0300

    LastStand

--- Phrase.jl
+++ Phrase.jl
@@ -1,9 +1,23 @@
-function myreverse(name)
-    for n in string
+#Без ввода
+function myreverse1(name)
+    name = ""
+    for n in name
         name = string(n,name)
     end
     return name
 end
 
-myreverse("sabaka")
-myreverse("MagadaN")
+myreverse1("name")
+myreverse1("sabaka")
+myreverse1("MagadaN")
+
+#С вводом
+function myreverse1(name)
+    name = readline()
+    for n in name
+        name = string(n,name)
+    end
+    return name
+end
+println("Введитие нужно вам слово два раза\n")
+myreverse1("name")
