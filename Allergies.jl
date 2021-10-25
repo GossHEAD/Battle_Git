e56da628f4c128242f98da1619100ed7705b5173
GossHEAD <bagdachov@gmail.com>
2021-10-25 22:15:14 +0300

    NotAllerg

--- Allergies.jl
+++ Allergies.jl
@@ -1,1 +1,10 @@
-function allerg()
+allns_all = Dict("eggs" => 1, "peanuts" => 2, "shellfish" => 4, "strawberries" => 8,
+"tomatoes" => 16, "chocolate" => 32, "pallen" => 64, "cats" => 128)
+
+function allns_to(points, allergen)
+    allergen in allns_list(points)
+end
+
+function allns_list(points)
+        #Дальше начинается какая-то проверка 
+end
