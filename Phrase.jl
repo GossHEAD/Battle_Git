5aa4132b65a0efe8e61cd60b3090293865181a6e
GossHEAD <bagdachov@gmail.com>
2021-10-25 21:21:10 +0300

    AllForOne

--- Phrase.jl
+++ Phrase.jl
@@ -1,23 +1,27 @@
-#Без ввода
-function myreverse1(name)
-    name = ""
-    for n in name
-        name = string(n,name)
-    end
-    return name
-end
+# #Без ввода
+# global name_reverse = ""
+# function myreverse1(name)
+#     for n in name
+#         global name_reverse = string(n, name)    # 1 2 3 4 5... 4321
+#     end
+#     #name_reverse[1]=""
+# return name_reverse
+# end
 
+# myreverse1("name")
+# myreverse1("sabaka")
+# myreverse1("MagadaN")
+
+
+
+
+function myreverse1(name)
+    y = ""
+    for n = 1:length(name)
+        y = string(n,name)
+    end
+    return y
+end
 myreverse1("name")
 myreverse1("sabaka")
 myreverse1("MagadaN")
-
-#С вводом
-function myreverse1(name)
-    name = readline()
-    for n in name
-        name = string(n,name)
-    end
-    return name
-end
-println("Введитие нужно вам слово два раза\n")
-myreverse1("name")
