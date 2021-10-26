bd9f9acbaaf1f4cdaf53a555c3ca1f3833e153a8
GossHEAD <bagdachov@gmail.com>
2021-10-26 21:52:31 +0300

    Raindrops

--- Raindrops-1.jl
+++ Raindrops-1.jl
@@ -6,6 +6,15 @@ function raindrop(score::Int)
     if mod(score, 5) == 0
         out *= "Plang"
     end
-    if mod(score, 3) == 0
-        out *= "Pling"
+    if mod(score, 7) == 0
+        out *= "Plong"
     end
+    if out != ""
+        return out
+    else
+       return string(score)
+    end
+end
+
+raindrop(30)
+
